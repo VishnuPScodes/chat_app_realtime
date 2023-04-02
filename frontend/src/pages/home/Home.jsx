4731ed38ab26d70a24cc9694b5b0e01ffd023fd3
VishnuPS <psvishnu373@gmail.com>
2023-04-02 19:32:06 +0530

    added file for the users page

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,17 +1,44 @@
 import styles from './home.module.css';
-
+import io from "socket.io-client";
+import { useState } from 'react';
+import { useEffect } from 'react';
 
 
 
 export const Home=()=>{
+    const [messages, setMessages] = useState([]);
+    const [newMessage, setNewMessage] = useState("");
+    const socket = io("http://localhost:2000");
+    useEffect(() => {
+      // Listen for incoming messages
+      console.log('use effect called');
+        socket.off("direct message");
+      socket.on("direct message", (message) => {
+        setMessages((prevMessages) => [...prevMessages, message]);
+      });
+    }, [socket]);
+  const handleSend=()=>{
+   
+    socket.emit("direct message", {
+      sender: "64267dbb1fbd8d7c5baf8c25",
+      reciever: "64267dbb1fbd8d7c5baf8c25",
+      message: newMessage,
+    });
+    setNewMessage("");
+  }
+  console.log('message',messages);
+  
     return (
       <div>
         <div className={styles.cont}>
           <div className={styles.display}>
-            <div className={styles.single_chat}>
-              heysddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd
-            </div>
+           
             <div className={styles.single_chat}>hey</div>
+            {messages.map((e)=>{
+              return <div>
+                {e.message}
+              </div>
+            })}
             
           </div>
           <div className={styles.btn_cont}>
@@ -19,8 +46,11 @@ export const Home=()=>{
               className={styles.btn_input}
               type="text"
               placeholder="Type your message here..."
+              onChange={(e)=>{
+                setNewMessage(e.target.value);
+              }}
             />
-            <button>Send</button>
+            <button onClick={handleSend}>Send</button>
           </div>
         </div>
       </div>
---
+++ frontend/src/pages/users/Users.jsx
@@ -0,0 +1,8 @@
+
+import styles from './user.module.css';
+
+export const Users=()=>{
+    return <div>
+
+    </div>
+}
